efc0cba6771eb3b0c2ad62de09fbd4d81fbf8f99
guswn3717 <rudals9973@gmail.com>
2025-01-31 19:45:43 +0900

    20250131템플릿

--- Class09th/FileName.cpp
+++ Class09th/FileName.cpp
@@ -14,26 +14,103 @@ public:
 		this->y = y;
 	}
 	
-	const int& x()
+	const int& X() { return x; }
+	const int& Y() { return y; }
+
+	Vector2& operator + (const Vector2& clone)
 	{
-		return x;
+		Vector2 vector(x + clone.x, y + clone.y);
+
+		return vector;
 	}
 
-	const int& y()
+	Vector2& operator - (const Vector2& clone)
 	{
-		return y;
+		Vector2 vector(x - clone.x, y - clone.y);
+
+		return vector;
+	}
+
+	Vector2& operator * (const Vector2& clone)
+	{
+		Vector2 vector(x * clone.x, y * clone.y);
+
+		return vector;
+	}
+
+	Vector2& operator / (const Vector2& clone)
+	{
+		Vector2 vector(x / clone.x, y / clone.y);
+
+		return vector;
+	}
+
+	Vector2& operator++()
+	{
+		this->x++;
+		this->y++;
+
+		return *this;
+	}
+
+	Vector2& operator--()
+	{
+		this->x--;
+		this->y--;
+
+		return *this;
+	}
+
+	Vector2& operator++(int)
+	{
+		Vector2 clone(x, y);
+
+		this->x++;
+		this->y++;
+
+		return *this;
+	}
+
+	Vector2& operator--(int)
+	{
+		Vector2 clone(x, y);
+
+		this->x--;
+		this->y--;
+
+		return *this;
 	}
 };
+
 
 int main()
 {
 #pragma region 연산자 오버로딩
 	
-	Vector2 direction1(1, 1);
-	Vector2 direction2(5, 5);
+	Vector2 direction1(3, 4);
+	Vector2 direction2(1, 2);
 
 	Vector2 direction = direction1 + direction2;
+	cout << direction.X() << " " << direction.Y() << endl;
 
+	direction = direction1 - direction2;
+	cout << direction.X() << " " << direction.Y() << endl;
+	
+	direction = direction1 * direction2;
+	cout << direction.X() << " " << direction.Y() << endl;
+	
+	direction = direction1 / direction2;
+	cout << direction.X() << " " << direction.Y() << endl;
+
+	++direction;
+	cout << direction.X() << " " << direction.Y() << endl;
+
+	--direction;
+	cout << direction.X() << " " << direction.Y() << endl;
+
+	cout << direction++.X() << endl;
+
+	cout << direction.X() << " " << direction.Y() << endl;
 
 #pragma endregion
 
