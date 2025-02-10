aa4c0a4854dfdb0e9eb9979eecfcc978d023c69c
guswn3717 <149389998+guswn3717@users.noreply.github.com>
2025-02-10 19:39:54 +0900

    20250210 연관컨테이너

--- Class14th/FileName.cpp
+++ Class14th/FileName.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stack>
+#include<Queue>
 
 using namespace std;
 
@@ -8,17 +9,37 @@ int main()
 #pragma region stack
 	std::stack<int> stack;
 
-	stack.push(10);
-	stack.push(20);
-	stack.push(30);
-	
-	cout << "Stack Size : " << stack.size() << endl;
+	//stack.push(10);
+	//stack.push(20);
+	//stack.push(30);
+	//
+	//cout << "Stack Size : " << stack.size() << endl;
+	//
+	//while (stack.empty() == false)
+	//{
+	//	cout << stack.top() << endl;
+	//
+	//	stack.pop();
+	//}
 
-	while (stack.empty() == false)
+#pragma endregion
+
+#pragma region Queue
+	std::queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	queue.push(40);
+	queue.push(50);
+
+	cout << "Queue Size : " << queue.size() << endl;
+
+	while (queue.empty() == false)
 	{
-		cout << stack.top() << endl;
+		cout << queue.front() << endl;
 
-		stack.pop();
+		queue.pop();
 	}
 
 #pragma endregion
